c44d3056889ef62bd1f1d533bf9ab97b0c988b9f
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-25 20:52:15 -0300

    update

--- computercraft/src/main.lua
+++ computercraft/src/main.lua
@@ -1,50 +1,14 @@
 --------------------------------------------------------------------------------
--- ENERGY TESTE 
+-- GERADOR MAIN 
 --------------------------------------------------------------------------------
 
--- local inductionMatrix = peripheral.wrap("inductionPort_0")
--- print(inductionMatrix)
--- print(inductionMatrix.getEnergy())
+shell.run("/CC/computercraft/src/GeneratorManager/Generator.lua")
 
 --------------------------------------------------------------------------------
--- GUI REDDIT
+-- GERADOR GUI 
 --------------------------------------------------------------------------------
 
-local basalt = require("/CC/computercraft/Modules/basalt")
-local main = basalt.createFrame()
-local balScreen = basalt.createFrame()
-local drive = peripheral.wrap("bottom")
-local myThread = main:addThread()
-balScreen:hide()
- 
-function makeMainScreen()
-    balScreen:hide()
-    local aLabel = main:addLabel()
-    aLabel:setFontSize(1)
-    aLabel:setText("Insert your card")
-    aLabel:setPosition(4, 4)
-    main:show()
-end
-function makeBalScreen()
-    main:hide()
-    local aLabel = balScreen:addLabel()
-    aLabel:setText("Your bal is 100000") 
-    aLabel:setFontSize(2)
-    balScreen:show()
-end
-
-while true do
-    if drive.isDiskPresent() then
-        makeBalScreen()
-        basalt.autoUpdate()
-    else
-        makeMainScreen()
-        basalt.autoUpdate()
-    end
-
-    basalt.autoUpdate()
-    os.sleep(0.05)
-end
+-- shell.run("/CC/computercraft/src/GeneratorManager/GUI.lua")
 
 --------------------------------------------------------------------------------
 -- GUI TESTE
