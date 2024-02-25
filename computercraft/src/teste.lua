881fef4db894da9be18c3ab29ab1842ebb9077e4
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-25 14:51:45 -0300

    add

---
+++ computercraft/src/teste.lua
@@ -0,0 +1,47 @@
+-- integrator = peripheral.find("redstoneIntegrator")
+-- if integrator == nil then error("not found") end
+-- print(integrator.getAnalogInput("north"))
+
+
+local inductionMatrix = peripheral.wrap("inductionPort_0")
+print(inductionMatrix)
+print(inductionMatrix.getEnergy())
+print(inductionMatrix.getEnergyCapacity())
+
+
+-- local basalt = require("basalt")
+-- local main = basalt.createFrame()
+-- local balScreen = basalt.createFrame()
+-- local drive = peripheral.wrap("bottom")
+-- local myThread = main:addThread()
+-- balScreen:hide()
+ 
+-- function makeMainScreen()
+--     balScreen:hide()
+--     local aLabel = main:addLabel()
+--     aLabel:setFontSize(1)
+--     aLabel:setText("Insert your card")
+--     aLabel:setPosition(4, 4)
+--     main:show()
+-- end
+-- function makeBalScreen()
+--     main:hide()
+--     local aLabel = balScreen:addLabel()
+--     aLabel:setText("Your bal is 100000") 
+--     aLabel:setFontSize(2)
+--     balScreen:show()
+-- end
+
+-- while true do
+--     if drive.isDiskPresent() then
+--         makeBalScreen()
+--         basalt.autoUpdate()
+--     else
+--         makeMainScreen()
+--         basalt.autoUpdate()
+--     end
+
+--     basalt.autoUpdate()
+--     os.sleep(0.05)
+-- end
+
