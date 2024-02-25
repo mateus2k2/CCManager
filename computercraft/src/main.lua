dda9039ca4f7e8e67cf42c66e5fab4e00a6155bf
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-25 20:22:46 -0300

    update

--- computercraft/src/main.lua
+++ computercraft/src/main.lua
@@ -10,44 +10,44 @@
 -- GUI REDDIT
 --------------------------------------------------------------------------------
 
--- local basalt = require("basalt")
--- local main = basalt.createFrame()
--- local balScreen = basalt.createFrame()
--- local drive = peripheral.wrap("bottom")
--- local myThread = main:addThread()
--- balScreen:hide()
+local basalt = require("/CC/computercraft/Modules/basalt")
+local main = basalt.createFrame()
+local balScreen = basalt.createFrame()
+local drive = peripheral.wrap("bottom")
+local myThread = main:addThread()
+balScreen:hide()
  
--- function makeMainScreen()
---     balScreen:hide()
---     local aLabel = main:addLabel()
---     aLabel:setFontSize(1)
---     aLabel:setText("Insert your card")
---     aLabel:setPosition(4, 4)
---     main:show()
--- end
--- function makeBalScreen()
---     main:hide()
---     local aLabel = balScreen:addLabel()
---     aLabel:setText("Your bal is 100000") 
---     aLabel:setFontSize(2)
---     balScreen:show()
--- end
+function makeMainScreen()
+    balScreen:hide()
+    local aLabel = main:addLabel()
+    aLabel:setFontSize(1)
+    aLabel:setText("Insert your card")
+    aLabel:setPosition(4, 4)
+    main:show()
+end
+function makeBalScreen()
+    main:hide()
+    local aLabel = balScreen:addLabel()
+    aLabel:setText("Your bal is 100000") 
+    aLabel:setFontSize(2)
+    balScreen:show()
+end
 
--- while true do
---     if drive.isDiskPresent() then
---         makeBalScreen()
---         basalt.autoUpdate()
---     else
---         makeMainScreen()
---         basalt.autoUpdate()
---     end
+while true do
+    if drive.isDiskPresent() then
+        makeBalScreen()
+        basalt.autoUpdate()
+    else
+        makeMainScreen()
+        basalt.autoUpdate()
+    end
 
---     basalt.autoUpdate()
---     os.sleep(0.05)
--- end
+    basalt.autoUpdate()
+    os.sleep(0.05)
+end
 
 --------------------------------------------------------------------------------
 -- GUI TESTE
 --------------------------------------------------------------------------------
 
-shell.run("/CC/computercraft/src/tests/guiTest/menager.lua")
+-- shell.run("/CC/computercraft/src/tests/guiTest/menager.lua")
