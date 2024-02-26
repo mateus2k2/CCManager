efd1a0813b00154ca429bfdb9819e71c6542c07e
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 09:17:26 -0300

    update

--- computerCraft/src/main.lua
+++ computerCraft/src/main.lua
@@ -21,46 +21,5 @@
 --------------------------------------------------------------------------------
 
 
--- ./CCGit-1.2/files/Shell/git "get" "mateus2k2" "CCManager" "master" "computerCraft/Modules" "teste"
-
--- Function to fetch contents of a URL
-local function fetch(url)
-    local response = http.get(url)
-    if response then
-        local content = response.readAll()
-        response.close()
-        return content
-    else
-        error("Failed to fetch URL: " .. url)
-    end
-end
-
--- Function to extract filenames from HTML content
-local function extractFilenames(htmlContent)
-    local filenames = {}
-    for filename in htmlContent:gmatch('href="([^"]+)"') do
-        if filename ~= "../" then -- Exclude parent directory link
-            table.insert(filenames, filename)
-        end
-    end
-    return filenames
-end
-
--- Function to get all files in a given path of a Git repository
-local function getFilesFromGitRepo(repoURL, path)
-    local url = repoURL .. "/tree/master/" .. path
-    local htmlContent = fetch(url)
-    local filenames = extractFilenames(htmlContent)
-    return filenames
-end
-
--- Example usage
-local repoURL = "https://github.com/mateus2k2/CCManager"
-local path = "computerCraft" -- Specify the path within the repository
-local files = getFilesFromGitRepo(repoURL, path)
-
--- Print the list of files
-print("Files in '" .. path .. "':")
-for _, filename in ipairs(files) do
-    print(filename)
-end
+-- ./CCGit-1.2/files/Shell/git "get" "mateus2k2" "CCManager" "master/computerCraft" "computerCraft/Modules" "teste"
+-- ./github clone mateus2k2/CCManager -b master CC mateus2k2
