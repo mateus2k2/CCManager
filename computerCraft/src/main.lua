7bec26d2c3025d6a7b1a540dc772e6c7f6c8c7b8
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 09:52:24 -0300

    update

--- computerCraft/src/main.lua
+++ computerCraft/src/main.lua
@@ -20,6 +20,4 @@
 -- REPO TESTE
 --------------------------------------------------------------------------------
 
-
--- ./CCGit-1.2/files/Shell/git "get" "mateus2k2" "CCManager" "master/computerCraft" "computerCraft/Modules" "teste"
--- ./github clone mateus2k2/CCManager -b master CC mateus2k2
+print("Iniciando teste de repositorio")
