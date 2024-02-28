e1cb8afa586154ed67199a56a1f953f7c0cdc15c
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-28 12:05:47 -0300

    update

--- server/src/server.js
+++ server/src/server.js
@@ -1,41 +1,41 @@
-const WebSocket = require('ws');
-const readline = require('readline');
+// const WebSocket = require('ws');
+// const readline = require('readline');
 
-const socketPort = 5000;
-const wss = new WebSocket.Server({ port: socketPort });
+// const socketPort = 5000;
+// const wss = new WebSocket.Server({ port: socketPort });
 
-wss.on('connection', function connection(ws) {
-  console.log('Client connected');
+// wss.on('connection', function connection(ws) {
+//   console.log('Client connected');
 
-  ws.on('message', function incoming(message) {
-    console.log('Received message from client:', message);
+//   ws.on('message', function incoming(message) {
+//     console.log('Received message from client:', message);
     
-    ws.send('Echo from server: ' + message);
-  });
+//     ws.send('Echo from server: ' + message);
+//   });
 
-  ws.on('close', function close() {
-    console.log('Client disconnected');
-  });
-});
+//   ws.on('close', function close() {
+//     console.log('Client disconnected');
+//   });
+// });
 
-console.log('WebSocket server running on port', socketPort);
+// console.log('WebSocket server running on port', socketPort);
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-rl.setPrompt('Type a message to send to clients: ');
+// rl.setPrompt('Type a message to send to clients: ');
 
-rl.on('line', function (input) {
-  wss.clients.forEach(function (client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(input);
-    }
-  });
-});
+// rl.on('line', function (input) {
+//   wss.clients.forEach(function (client) {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(input);
+//     }
+//   });
+// });
 
-rl.prompt();
+// rl.prompt();
 
 
 
@@ -54,34 +54,36 @@ rl.prompt();
 
 
 
-// const WebSocket = require('ws');
+const WebSocket = require('ws');
 
-// const wss = new WebSocket.Server({ port: 5001 }); // Create WebSocket server on port 8080
+const wss = new WebSocket.Server({ port: 5000 }); // Create WebSocket server on port 8080
 
-// let isClientConnected = false; // Flag to track if a client is already connected
+let isClientConnected = false; // Flag to track if a client is already connected
 
-// wss.on('connection', (ws) => {
-//   if (isClientConnected) {
-//     // If a client is already connected, reject new connection
-//     ws.close(1000, 'Only one connection allowed');
-//     return;
-//   }
+wss.on('connection', (ws) => {
+  console.log('Client connected');
 
-//   ws.on('message', (message) => {
-//     if (message === '123456789') {
-//       // If the received message is the authentication token, allow the connection
-//       isClientConnected = true;
-//       ws.send('Authentication successful. You are connected.');
-//     } else {
-//       // If the received message is not the authentication token, close the connection
-//       ws.close(1000, 'Authentication failed. Closing connection.');
-//     }
-//   });
+  if (isClientConnected) {
+    // If a client is already connected, reject new connection
+    ws.close(1000, 'Only one connection allowed');
+    return;
+  }
 
-//   ws.on('close', () => {
-//     isClientConnected = false; // Reset the connection flag when the client disconnects
-//   });
-// });
+  ws.on('message', (message) => {
+    if (message === '123456789') {
+      // If the received message is the authentication token, allow the connection
+      isClientConnected = true;
+      ws.send('Authentication successful. You are connected.');
+    } else {
+      // If the received message is not the authentication token, close the connection
+      ws.close(1000, 'Authentication failed. Closing connection.');
+    }
+  });
 
-// console.log('WebSocket server running on port 5001');
+  ws.on('close', () => {
+    isClientConnected = false; // Reset the connection flag when the client disconnects
+  });
+});
+
+console.log('WebSocket server running on port 5000');
 
