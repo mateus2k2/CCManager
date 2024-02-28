3104e50bf7ee05d6023c26cd9a3c56f600ecae20
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-28 12:08:34 -0300

    update

--- server/src/server.js
+++ server/src/server.js
@@ -54,15 +54,60 @@
 
 
 
+// const WebSocket = require('ws');
+
+// const wss = new WebSocket.Server({ port: 5000 }); // Create WebSocket server on port 8080
+
+// let isClientConnected = false; // Flag to track if a client is already connected
+
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+
+//   if (isClientConnected) {
+//     // If a client is already connected, reject new connection
+//     ws.close(1000, 'Only one connection allowed');
+//     return;
+//   }
+
+//   ws.on('message', (message) => {
+//     if (message === '123456789') {
+//       // If the received message is the authentication token, allow the connection
+//       isClientConnected = true;
+//       ws.send('Authentication successful. You are connected.');
+//     } else {
+//       // If the received message is not the authentication token, close the connection
+//       ws.close(1000, 'Authentication failed. Closing connection.');
+//     }
+//   });
+
+//   ws.on('close', () => {
+//     isClientConnected = false; // Reset the connection flag when the client disconnects
+//   });
+// });
+
+// console.log('WebSocket server running on port 5000');
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const http = require('http');
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 5000 }); // Create WebSocket server on port 8080
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 let isClientConnected = false; // Flag to track if a client is already connected
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-
+  
   if (isClientConnected) {
     // If a client is already connected, reject new connection
     ws.close(1000, 'Only one connection allowed');
@@ -85,5 +130,10 @@ wss.on('connection', (ws) => {
   });
 });
 
-console.log('WebSocket server running on port 5000');
+app.get('/api', (req, res) => {
+  res.send('API endpoint');
+});
 
+server.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
